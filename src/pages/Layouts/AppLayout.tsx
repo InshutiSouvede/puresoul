20c6562226128ae7be922a53368fd58cfbded21c
InshutiSouvede <isouvede2@gmail.com>
2025-04-02 15:40:26 +0200

    fix: remove unnecessary isloggedin on navbar

--- src/pages/Layouts/AppLayout.tsx
+++ src/pages/Layouts/AppLayout.tsx
@@ -4,7 +4,7 @@ import NavBar from "../../components/NavBar";
 export default function AppLayout() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-purple-100">
-        <NavBar isLoggedIn = {false} />
+        <NavBar />
         <Outlet />
         <footer className="text-center mt-12 text-gray-600">
         <p>© 2025 PureSoul. Your Mental Health Matters.</p>
